dbbf428351594ba3073591ef9b4f3cbecdf06e1d
2541-MukaiNene <kmc2541@kamiyama.ac.jp>
2025-10-28 11:29:31 +0900

    Update ver 1.0.0

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/2541-MukaiNene/homebrew-dote"
   url "https://raw.githubusercontent.com/2541-MukaiNene/homebrew-dote/main/dote.c"
   sha256 "5257dd2942578c75cf72952cd8fde661315591a3146df91a3ce90a8b6cf14c52"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
