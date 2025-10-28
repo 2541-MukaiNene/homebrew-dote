beefeef8b2d5c085725c96f1bdea1b0ca8eafd3b
2541-MukaiNene <kmc2541@kamiyama.ac.jp>
2025-10-28 11:17:21 +0900

    Update SHA256 checksum

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/2541-MukaiNene/homebrew-dote"
   url "https://raw.githubusercontent.com/2541-MukaiNene/homebrew-dote/main/dote.c"
-  sha256 "PUT_SHA256_OF_DOTE.C_HERE"
+  sha256 "5257dd2942578c75cf72952cd8fde661315591a3146df91a3ce90a8b6cf14c52"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
